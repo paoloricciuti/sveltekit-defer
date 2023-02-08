e4fe9f519b9cbd85c81433f2bbb9ec9a9bcfb3ce
Paolo Ricciuti <ricciutipaolo@gmail.com>
2023-02-08 17:58:52 +0100

    Fix vite plugin

--- src/lib/vite/index.ts
+++ src/lib/vite/index.ts
@@ -1,6 +1,7 @@
 import type { SveltekitDeferOptions } from '$lib/constants';
 import type { Plugin } from 'vite';
 import MagicString from 'magic-string';
+import fs from 'fs';
 
 export const env_parser: {
 	[K in keyof SveltekitDeferOptions]?: (
@@ -13,28 +14,63 @@ export const env_parser: {
 	}
 };
 
+function fix_code(code: string, options: SveltekitDeferOptions) {
+	if (code.startsWith('/// @sveltekit-defer-constants')) {
+		const magicCode = new MagicString(code);
+		Object.keys(options).forEach((_key) => {
+			const key = _key as keyof typeof options;
+			const to_replace = env_parser[key]?.(options[key]) ?? options[key];
+			if (to_replace) {
+				magicCode.replace(
+					new RegExp(`${key}:\\s\\'.*\\',?\\r?\\n`),
+					`${key}: '${to_replace}',\r\n`
+				);
+			}
+		});
+		return {
+			code: magicCode.toString(),
+			map: magicCode.generateMap()
+		};
+	}
+}
+
 export function sveltekit_defer(options: SveltekitDeferOptions): Plugin {
 	return {
 		name: 'vite-plugin-sveltekit-defer',
+		config() {
+			return {
+				optimizeDeps: {
+					force: true,
+					esbuildOptions: {
+						plugins: [
+							{
+								name: 'esbuild-plugin',
+								setup(build) {
+									build.onLoad(
+										{
+											filter: /constants/
+										},
+										({ path }) => {
+											if (!path.includes('sveltekit-defer')) return;
+											const code = fs.readFileSync(path, 'utf-8');
+											const contents = fix_code(code, options)?.code;
+											return contents
+												? {
+														contents
+												  }
+												: undefined;
+										}
+									);
+								}
+							}
+						]
+					}
+				}
+			};
+		},
 		transform(code, id) {
 			if (id.endsWith('node_modules/sveltekit-defer/constants.js')) {
-				if (code.startsWith('/// @sveltekit-defer-constants')) {
-					const magicCode = new MagicString(code);
-					Object.keys(options).forEach((_key) => {
-						const key = _key as keyof typeof options;
-						const to_replace = env_parser[key]?.(options[key]) ?? options[key];
-						if (to_replace) {
-							magicCode.replace(
-								new RegExp(`${key}:\\s\\'.*\\',?\\r?\\n`),
-								`${key}: '${to_replace}',\r\n`
-							);
-						}
-					});
-					return {
-						code: magicCode.toString(),
-						map: magicCode.generateMap()
-					};
-				}
+				return fix_code(code, options);
 			}
 		}
 	};
